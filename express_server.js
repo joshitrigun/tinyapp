996018b6fce01d18cc1a5c879ea77525762dfe1c
Trigun Joshi <trigunjoshi@gmail.com>
2021-12-06 17:27:29 -0800

    additional changes

---
+++ express_server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const PORT = 9090;
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  bz24: "http://www.megtechsoft.com",
+  bbox: "http://bboxtech.com",
+  par212: "http://google.com",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
