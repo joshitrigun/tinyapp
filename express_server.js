53dba5a9632c3f36e5d157b7ee8cc4f06d4709be
Trigun Joshi <trigunjoshi@gmail.com>
2021-12-07 19:26:21 -0800

    new login changes added

--- express_server.js
+++ express_server.js
@@ -74,6 +74,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//login to express server
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
